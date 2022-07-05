574e2a2d692f02b7da4018835a516a997c502118
Averanc3 <xxdariusxx@yandex.ru>
2022-07-05 16:17:52 +0300

    "no" answer is fixed

--- PickTheQuestion.py
+++ PickTheQuestion.py
@@ -1,25 +1,31 @@
 import csv
 import random
 
+def askyn():
+    while True:
+        x = int(input('Number of questions printed: '))
+        if x <= 0:
+            print('Go perform your testing stuff somewhere else')
+        with open('questions_list.csv', newline='') as cf:
+            question_reader = csv.reader(cf)
+            m = list(question_reader)
+            repeat = []
+            while x > len(repeat):
+                z = m[random.randint(1, len(m))]
+                if z in repeat:
+                    continue
+                else:
+                    repeat.append(z)
 
-while True:
-    x = int(input('Number of questions printed: '))
-    if x <= 0:
-        print('Go perform your testing stuff somewhere else')
-    with open('questions_list.csv', newline='') as cf:
-        question_reader = csv.reader(cf)
-        m = list(question_reader)
-        repeat = []
-        while x > len(repeat):
-            z = m[random.randint(1, len(m))]
-            if z in repeat:
-                continue
-            else:
-                repeat.append(z)
+                print('Q',z[0],':',z[1],'\n ')
 
-            print('Q',z[0],':',z[1],'\n ')
-    l = input('More questions? (y/n): ')
-    if l == 'y':
-        continue
-    else:
-        break
+        l = input('More questions? (y/n): ')
+        if l == 'y':
+            continue
+        elif l == 'n':
+            return False
+        else:
+            print("Que?")
+
+
+askyn()
